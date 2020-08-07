c7d67eb60ca3bb873b6b2c462f8ad2c9f5f4c943
PrimozSmog <primoz.smogavec@gmail.com>
2020-08-07 14:05:13 +0200

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -113,7 +113,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_SPI3_Init();
-  //MX_UART4_Init();
+  MX_UART4_Init();
   MX_I2C1_Init();
   MX_SPI2_Init();
   MX_SPI1_Init();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # STM32F405_DJI_rpi_shield
  
+
+PCB Changes:
+relocation of uart4_rx -> usart2_rx
+
+RPI interrupt pin
